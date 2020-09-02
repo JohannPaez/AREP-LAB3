b0ceb69d5465d337ac56100c53e02b0d105b1125
Johann Paez <najoh2907@hotmail.com>
2020-09-02 12:57:42 -0500

    heroku M

--- src/main/java/com/arep/server/ReadWriteRequest.java
+++ src/main/java/com/arep/server/ReadWriteRequest.java
@@ -45,6 +45,7 @@ public class ReadWriteRequest {
 		String line = null;
 		String res = "";
 		try {
+			System.out.println("INLINE " + in.readLine());
 			while ((line = in.readLine()).length() != 0) {
 				res += line + "\n";
 				if (!in.ready()) break;
